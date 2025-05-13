d3ed28f6c0150b5aa290aae6a6b4ed6e88011640
Enock Joseph Mwesigwa <162899099+ejmwesigwa@users.noreply.github.com>
2025-05-13 17:53:59 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,12 +5,12 @@ from torchvision import transforms, models
 from PIL import Image
 import numpy as np
 import os
-from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
+from grad_cam import GradCAM
+from grad_cam.utils.model_targets import ClassifierOutputTarget
+from grad_cam.utils.image import show_cam_on_image
 
 # --- CONFIG ---
-MODEL_PATH = '/models/best_resnet_model.pth'
+MODEL_PATH = 'models/best_resnet_model.pth'
 CLASS_NAMES = [
     "Apple - Healthy", "Apple - Rotten",
     "Banana - Healthy", "Banana - Rotten",
